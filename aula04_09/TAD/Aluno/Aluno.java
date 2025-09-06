da838a8c5651d04d107312880a9fd10ded5be794
s0uzqw <luis.ribeiro2@estudante.ifms.edu.br>
2025-09-05 20:47:07 -0400

    Update TAD Aluno

---
+++ aula04_09/TAD/Aluno/Aluno.java
@@ -0,0 +1,14 @@
+package Aluno;
+
+import java.util.Scanner;
+
+public class Aluno {
+    Scanner entrada = new Scanner(System.in);
+
+    String aluno;
+    double notas[] = new double[3];
+
+    public void atribuirNota() {
+
+    }
+}
