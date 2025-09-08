3bb4c1340c3b55d6aa3c4538e913c23482eead31
s0uzqw <luis.ribeiro2@estudante.ifms.edu.br>
2025-09-08 16:54:19 -0300

    Update-Ending Aluno.java and ProgramaAluno.java

--- aula04_09/TAD/Aluno/Aluno.java
+++ aula04_09/TAD/Aluno/Aluno.java
@@ -6,9 +6,38 @@ public class Aluno {
     Scanner entrada = new Scanner(System.in);
 
     String aluno;
-    double notas[] = new double[3];
+    double notas[] = new double[4];
+
+    public void nomeAluno() {
+        System.out.println("\nInsira o nome do aluno: ");
+        this.aluno = entrada.nextLine();
+    }
 
     public void atribuirNota() {
+        System.out.println("Informe as 4 notas do aluno: ");
+        for (int i = 0; i < 4; i++) {
+            this.notas[i] = entrada.nextDouble();
+        }
+    }
 
+    public void mostrarNotas() {
+        for(int i = 0; i < 4; i++) {
+            System.out.printf("Nota %d são %.1f\n", i + 1, notas[i]);
+        }
+    }
+
+    double assistenteMedia = 0;
+    double media = 0;
+
+    public void calcularMedia() {
+        for(int i = 0; i < 4; i++) {
+            assistenteMedia += this.notas[i];
+            media = assistenteMedia / 4;
+        }
+        System.out.println("Média calculada!");
+    }
+
+    public void mostrarMedia() {
+        System.out.printf("A média de %s é de %.1f", aluno, media);
     }
 }
--- aula04_09/TAD/Aluno/ProgramaAluno.java
+++ aula04_09/TAD/Aluno/ProgramaAluno.java
@@ -1,5 +1,40 @@
 package Aluno;
 
+import java.util.Scanner;
+
 public class ProgramaAluno {
-    
+    public static void main(String[] args) {
+        Aluno al = new Aluno();
+
+        Scanner entrada = new Scanner(System.in);
+
+        int opcao = -1;
+
+        while (opcao != 5) {
+            System.out.println("\n\n0 - Inserir nome do aluno");
+            System.out.println("1 - Atribuir nota");
+            System.out.println("2 - Mostrar notas");
+            System.out.println("3 - Calcular media");
+            System.out.println("4 - Mostrar media");
+            System.out.println("5 - Sair\n");
+
+            opcao = entrada.nextInt();
+            if (opcao == 0) {
+                al.nomeAluno();
+            } else if (opcao == 1) {
+                al.atribuirNota();
+            } else if (opcao == 2) {
+                al.mostrarNotas();
+            } else if (opcao == 3) {
+                al.calcularMedia();
+            } else if (opcao == 4) {
+                al.mostrarMedia();
+            } else if (opcao == 5) {
+
+            } else {
+                System.out.println("Opção inválida");
+            }
+        }
+        entrada.close();
+    }
 }
